5285edbbd7b64ef4238b35c1b2ef79369efd087a
Amirreza Rashidi <amirrezarashidi5831ar@gmail.com>
2025-07-13 21:31:01 +0330

    first upload of oop in java

---
+++ src/e01/Animal.java
@@ -0,0 +1,12 @@
+package src.e01;
+public class Animal {
+int age;
+int weight;
+String name;
+
+public void Show(){
+    System.out.println("Animal name is " + name
+    + "  and age is " + age
+    + "  and weight is " + weight);
+}
+}
---
+++ src/e01/first.java
@@ -0,0 +1,20 @@
+package src.e01;
+import java.util.*;
+public class first {
+    public static void main(String[] args) {
+Scanner cin = new Scanner(System.in);
+
+for (int i=0; i<3; i++) {
+ Animal animal = new Animal();
+  System.out.println("Enter the name of the animal: ");
+  animal.name = cin.nextLine();
+  System.out.println("Enter the age of the animal: ");
+  animal.age = cin.nextInt();
+  System.out.println("Enter the weight of the animal: ");
+  animal.weight = cin.nextInt();
+  animal.Show();
+  cin.nextLine();
+}
+    }
+}
+// cause we dont save our objects    after for loop runing we dont have an object   we should use arrays to save our objects
